8d7c3a31ce6e6a67cc5882ae2e6ae04fc2e2e909
Phillip <rphillip.thomas@gmail.com>
2018-10-19 23:19:22 +0200

    WIP FLANN Matcher

--- Source/FeatureMatching/FLANNMatcher.cpp
+++ Source/FeatureMatching/FLANNMatcher.cpp
@@ -1,6 +1,22 @@
 #include "FLANNMatcher.h"
 
-FLANNMatcher::FLANNMatcher()
+FLANNMatcher::FLANNMatcher(QList<ImageContainer> &imageList, QList<FeatureContainer> &featureContainerList)
 {
+    this->imageList = imageList;
+    this->featureContainerList = featureContainerList;
+}
 
+void FLANNMatcher::StartMatching()
+{
+    if(imageList.count() != featureContainerList.count()) {
+        return;
+    }
+
+    for(int i = 0; i < imageList.count(); i++) {
+        ImageContainer imgContainer       = imageList.at(i);
+        FeatureContainer featureContainer = featureContainerList.at(i);
+
+       // cv::xfeatures2d::SIFT::create()
+
+    }
 }
--- Source/FeatureMatching/FLANNMatcher.h
+++ Source/FeatureMatching/FLANNMatcher.h
@@ -1,7 +1,25 @@
 #pragma once
 
+#include <QList>
+#include "Image/ImageContainer.h"
+#include "DTO/FeatureContainer.h"
+
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/xfeatures2d.hpp"
+
 class FLANNMatcher
 {
+private:
+    QList<ImageContainer> imageList;
+    QList<FeatureContainer> featureContainerList;
+
 public:
-    FLANNMatcher();
+    FLANNMatcher(QList<ImageContainer> &imageList, QList<FeatureContainer> &featureContainerList);
+
+public:
+    void StartMatching();
+
 };
