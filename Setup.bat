2359be4c69cd041cdedc6463dfa3be57f9041aa7
Phillip <rphillip.thomas@gmail.com>
2018-11-27 23:06:07 +0100

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -11,6 +11,6 @@ if not exist .\External\vcpkg\bootstrap-vcpkg.bat (
 
 cd .\External\vcpkg\
 
-vcpkg install magnum opencv[core,sfm,contrib] --triplet x64-windows
+vcpkg install magnum opencv[*] --triplet x64-windows
 
 pause
