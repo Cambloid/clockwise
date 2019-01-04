da7d46b4487bce88c1b22474383f9b04cbf185ea
Phillip <rphillip.thomas@gmail.com>
2019-01-04 23:44:02 +0100

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -7,10 +7,17 @@ echo. ********************
 if not exist .\External\vcpkg\bootstrap-vcpkg.bat (
 	git submodule update --init --recursive
 	call .\External\vcpkg\bootstrap-vcpkg.bat
+
+	cd .\External\vcpkg\
+	
+	vcpkg install glslang glm opencv[*] --triplet x64-windows
+
 )
 
-cd .\External\vcpkg\
+rem TODO: Compile shaders
 
-vcpkg install glslang glm opencv[*] --triplet x64-windows
+
+
+
 
 pause
