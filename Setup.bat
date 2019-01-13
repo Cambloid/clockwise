0344bb7952ff45990c70d604688f3a6c37159f44
Phillip <rphillip.thomas@gmail.com>
2019-01-13 15:05:54 +0100

    Update Setup.bat
    Added Time overview in Setup
    Added dependencies to vcpkg (vulkan, qt5)

--- Setup.bat
+++ Setup.bat
@@ -4,13 +4,16 @@ echo. ********************
 echo. *Setup Dependencies*
 echo. ********************
 
+set startDate=%date%
+set startTime=%time%
+
 if not exist .\External\vcpkg\bootstrap-vcpkg.bat (
 	git submodule update --init --recursive
 	call .\External\vcpkg\bootstrap-vcpkg.bat
 
 	cd .\External\vcpkg\
 	
-	vcpkg install qt5 glslang glm qt5 opencv[*] --triplet x64-windows
+	vcpkg install vulkan qt5 glslang glm opencv[*] --triplet x64-windows
 	
 	cd ..\..
 )
@@ -25,10 +28,20 @@ if not exist build_windows_vs17_win64 (
 	cmake -G "Visual Studio 15 Win64" "../CoreTrack/" -DCMAKE_TOOLCHAIN_FILE=".\External\vcpkg\scripts\buildsystems\vcpkg.cmake"
 )
 
+
+set endDate=%date%
+set endTime=%time%
+
 rem TODO: Compile shaders
 
-
-
-
+echo. ======================================
+echo. Time overview
+echo.  
+echo. Start date: %startDate% 
+echo. Start time: %startTime%
+echo.  
+echo. End date:   %endDate%
+echo. End time:   %endTime%
+echo. ======================================
 
 pause
